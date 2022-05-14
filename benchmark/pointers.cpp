8c1977ac98608495a522ef5e5c88c1c7c32496f9
oren12321 <oren12321@gmail.com>
2022-05-14 15:37:21 +0300

    Extend Shared_ptr benchmark

--- benchmark/pointers.cpp
+++ benchmark/pointers.cpp
@@ -7,16 +7,19 @@
 
 static void BM_std_shared_ptr(benchmark::State& state)
 {
-    std::array<std::shared_ptr<int>, 256> ptrs;
-    std::shared_ptr<int> p = std::make_shared<int>(0);
-
     for (auto _ : state) {
+        std::array<std::shared_ptr<int>, 256> ptrs;
+        std::shared_ptr<int> p = std::make_shared<int>(0);
         for (auto& ptr : ptrs) {
             ptr = p;
         }
-        //for (auto& ptr : ptrs) {
-        //    ptr.reset();
-        //}
+        for (auto& ptr : ptrs) {
+            auto n = ptr.use_count();
+            auto& x = *ptr;
+        }
+        for (auto& ptr : ptrs) {
+            ptr.reset();
+        }
     }
 }
 BENCHMARK(BM_std_shared_ptr);
@@ -25,16 +28,19 @@ static void BM_LW_shared_ptr(benchmark::State& state)
 {
     using namespace math::core::pointers;
 
-    std::array<Shared_ptr<int>, 256> ptrs;
-    Shared_ptr<int> p = Shared_ptr<int>::make_shared(0);
-
     for (auto _ : state) {
+        std::array<Shared_ptr<int>, 256> ptrs;
+        Shared_ptr<int> p = Shared_ptr<int>::make_shared(0);
         for (auto& ptr : ptrs) {
             ptr = p;
         }
-        //for (auto& ptr : ptrs) {
-        //    ptr.reset();
-        //}
+        for (auto& ptr : ptrs) {
+            auto n = ptr.use_count();
+            auto& x = *ptr;
+        }
+        for (auto& ptr : ptrs) {
+            ptr.reset();
+        }
     }
 }
 BENCHMARK(BM_LW_shared_ptr);
